36f458c60002631106e30d2255a2928e28bccf50
Grigore1 <popovicigrigore777@gmail.com>
2023-12-29 04:34:42 +0200

    modificari footer

--- src/components/FooterComponent/FooterComponent.tsx
+++ src/components/FooterComponent/FooterComponent.tsx
@@ -23,8 +23,8 @@ const FooterComponent = () => {
                         <p>Chisinau, Republic of Moldova</p>
                         <p>Customer Service 24/24</p>
                         <p>Fast Delivery & Money Back Guarantee</p>
-                        <p className="email-id">popovicigrigore777@gmail.com</p>
-                        <h4>+373 - 0123456789</h4>
+                        <p className="email-id mt-4">popovicigrigore777@gmail.com</p>
+                        <h4 className="mt-4">+373 - 0123456789</h4>
                     </div>
                     <div className="links flex-basis-25 p-4 ml-[-80px] mt-28">
                         <div className="align">
@@ -46,13 +46,12 @@ const FooterComponent = () => {
                             Info Desk
                             <div className=""><span></span></div>
                         </h3>
-                        <form className="pb-10 flex items-center justify-between border-b border-gray-300 mb-5">
                             <i className="far fa-envelope text-gray-400 text-2xl mr-4"></i>
                             <input type="email" placeholder="Enter your email id" className="w-full bg-transparent text-gray-400 border-0 outline-none mr-16" required />
                             <button type="submit" className="bg-transparent border-0 outline-none cursor-pointer">
                                 <i className="fas fa-arrow-right text-gray-400 text-2xl"></i>
                             </button>
-                        </form>
+                        <hr className="w-36 ml-4"></hr>
                         <div className="social-icons">
                             <i className="fab fa-facebook-f text-2xl w-16 h-16 rounded-full text-center leading-16 text-black bg-white mr-5 cursor-pointer"></i>
                             <i className="fab fa-instagram text-2xl w-16 h-16 rounded-full text-center leading-16 text-black bg-white mr-5 cursor-pointer"></i>
