1699c79bc95887888f9b23ef7c69cf960d6d82f1
Grigore1 <popovicigrigore777@gmail.com>
2024-01-03 02:12:57 +0200

    sectiune pe mainpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import FooterComponent from "./components/FooterComponent/FooterComponent";
+import StartSectionComponent from "./components/StartSectionComponent/StartSectionComponent";
 
 function App() {
   return (
     <div className="AGC">
         <FooterComponent />
+        <StartSectionComponent />
     </div>
   );
 }
