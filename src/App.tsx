57039989c244e32476d1569b93da92fac2f918a9
aclin143 <caliniustin12@gmail.com>
2024-01-12 23:25:49 +0300

    header curat, caruselul urmeaza

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
 import FooterComponent from "./components/FooterComponent/FooterComponent";
+
 
 function App() {
   return (
       <div className="AGC">
-          <h1>Muie</h1>
+          <HeaderComponent />
+
           <FooterComponent/>
+
       </div>
   );
 }
