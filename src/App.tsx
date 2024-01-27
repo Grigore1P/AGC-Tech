64108989265e6bd29e2b7307df13a822d96e451b
Grigore1 <popovicigrigore777@gmail.com>
2024-01-27 16:36:10 +0200

    learnitComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
 import FooterComponent from "./components/FooterComponent/FooterComponent";
+import StartSectionComponent from "./components/StartSectionComponent/StartSectionComponent";
+import LearnITComponent from "./components/LearnITComponent/LearnITComponent";
 
 
 function App() {
   return (
       <div className="AGC">
           <HeaderComponent />
+          <StartSectionComponent />
+          <LearnITComponent />
 
           <FooterComponent/>
 
--- src/components/LearnITComponent/LearnITComponent.tsx
+++ src/components/LearnITComponent/LearnITComponent.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 
+
 const LearnITComponent = () => {
-    <div className="">
-        
+    return(
+    <div className="learnit bg-black w-full h-fit relative">
+        <div className="content text-white font-sans ml-40">
+        <h3 className="text-xl font-bold"><br /> <br />De ce noi?</h3>
+        <p className="text-gray-400 w-64"> Suntem doi tineri entuziaști,
+            dedicați pasiunii noastre pentru domeniul IT și avem o experiență solidă
+            de 5 ani în acest domeniu dinamic. <br />
+            <br />
+            Împărtășim cu bucurie secretele noastre de succes și vă invităm să vă alăturați comunității AGC,
+            unde promovăm entuziasmul pentru tehnologie și dezvoltare personală în IT.
+            <br />
+            <br />
+            Începeți călătoria dumneavoastră captivantă alături de noi, AGC Tech!
+        </p>
+        </div>
+
     </div>
+    )
 }
 export default LearnITComponent;
