4bea33a5ed8f958ce3d816118a28d8c6e968a7af
Grigore1 <popovicigrigore777@gmail.com>
2024-02-17 19:27:43 +0200

    sectiune updatata, erori

--- src/components/LearnITComponent/LearnITComponent.tsx
+++ src/components/LearnITComponent/LearnITComponent.tsx
@@ -5,7 +5,7 @@ const LearnITComponent = () => {
     return(
     <div className="learnit bg-black w-full h-fit relative">
         <div className="content text-white font-sans ml-72">
-        <h3 className="text-xl font-bold"><br /> <br />De ce noi?</h3><br />
+        <h3 className="text-xl text-white font-bold"><br /> <br />AGC-Tech</h3><br />
         <p className="text-gray-400 w-64">
             Suntem doi tineri entuziaști,
             dedicați pasiunii noastre pentru domeniul IT și avem o experiență solidă
@@ -21,16 +21,18 @@ const LearnITComponent = () => {
             <br />
         </div>
 
-        <div className="us w-64 h-64 bg-[#5D3587] flex float-right mr-96 mt-[-350px] rounded-xl border-1 ">
-            <p className="text-black font-bold flex items-center justify-center ml-10">5+ ANI EXPERIENȚĂ<br />
-
-                SUPORT 24/24<br />
-
-                100% EFICIENȚĂ & CALITATE
-
-
-            </p>
+        <div className="us w-64 h-[400px] bg-[#57033F] font-sans flex float-right mr-96 mt-[-430px] rounded-xl border-2 border-[#F3D7CA]">
+            <div className="flex items-center justify-center mt-20">
+                <p className="text-white font-bold text-5xl flex items-center justify-center ml-10 mt-[-380px]">5+<br /></p>
+                <p className="text-black-800 font-bold flex  items-center justify-center ml-[-60px] mt-[-280px]">ANI EXPERIENȚĂ</p>
+                <p className="text-white font-bold text-5xl flex items-center justify-center ml-[-132px] mt-[-140px]">SUPORT <br /></p>
+                <p className="text-black-800 font-bold flex items-center justify-center ml-[-185px] mt-[-40px]">24/24 <br /></p>
+                <p className="text-white font-bold text-5xl flex items-center justify-center ml-[-51px] mt-24">100%</p>
+                <p className="text-black-800 font-bold flex items-center justify-center ml-[-117px] mt-[200px]">EFICIENȚĂ & CALITATE</p>
+            </div>
         </div>
+        <br />
+        <br />
     </div>
     )
 }
--- src/components/StartSectionComponent/StartSectionComponent.tsx
+++ src/components/StartSectionComponent/StartSectionComponent.tsx
@@ -1,26 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const StartSectionComponent = () => {
     const [isHovered, setIsHovered] = useState(false);
 
-    const videoUrl = 'https://player.vimeo.com/external/517093227.sd.mp4?s=eb426f7ae67097bf60da3c7386e4e468524166cb&profile_id=164&oauth2_token_id=57447761';
-
     return (
-        <div className={`w-full h-fit motivation relative mt-[-80px] ${isHovered ? 'hovered' : ''}`}>
+        <div className={`w-full h-fit motivation relative mt-[-80px] ${isHovered ? "hovered" : ""}`}>
             <div className="video-wrapper absolute inset-0 overflow-hidden rounded">
-                <video
-                    autoPlay
-                    loop
-                    muted
-                    className="object-cover w-full h-full"
-                >
-                    <source src={videoUrl} type="video/mp4" />
+                <video autoPlay loop muted className="object-cover w-full h-full">
+                    <source src="https://sebastianbotez.ro/videos/hero-video-programming-20.mp4" type="video/mp4" />
+                    <source src="https://sebastianbotez.ro/videos/hero-video-programming-20.webm" type="video/webm" />
+                    <source src="https://sebastianbotez.ro/videos/hero-video-programming-20.ogv" type="video/ogv" />
                     Your browser does not support the video tag.
                 </video>
             </div>
 
             <div className="content ml-40 mt-20 font-sans relative z-10 text-white">
-                <h3 className="text-base text-purple-800">AGC Tech - Tech Beyond Limits</h3>
+                <h3 className="text-base font-bold text-purple-800">AGC Tech - Tech Beyond Limits</h3>
                 <h1 className="text-3xl mt-6 font-bold">
                     Ești în căutarea unui ajutor specializat pentru a-ți alege<br />
                     cariera în domeniul IT?
@@ -32,8 +27,8 @@ const StartSectionComponent = () => {
                 <button
                     className={`w-32 h-12 text-white rounded-xl mt-10 transition-all ${
                         isHovered
-                            ? 'bg-gradient-to-l from-[#00052D] to-[#57033F]'
-                            : 'bg-gradient-to-r from-[#00052D] to-[#57033F]'
+                            ? "bg-gradient-to-l from-[#00052D] to-[#57033F]"
+                            : "bg-gradient-to-r from-[#00052D] to-[#57033F]"
                     }`}
                     onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}
