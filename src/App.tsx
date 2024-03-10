d4eac6f365710d955551a9c50a3ba8baf3cb71f2
aclin143 <caliniustin12@gmail.com>
2024-03-10 16:01:13 +0300

    icon section

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,8 @@ import Carousel from "./components/carusel.component";
 import FooterComponent from "./components/FooterComponent/FooterComponent";
 import StartSectionComponent from "./components/StartSectionComponent/StartSectionComponent";
 import LearnITComponent from "./components/LearnITComponent/LearnITComponent";
-
+import ScopComponent from "./components/ScopComponent/ScopComponent";
 function App() {
-  return (
-      <div className="AGC">
-          <HeaderComponent />
-          <StartSectionComponent />
-          <LearnITComponent />
-          <FooterComponent/>
     let slides = [
         "https://i.pinimg.com/originals/51/82/ac/5182ac536727d576c78a9320ac62de30.jpg",
         "https://wallpapercave.com/wp/wp3386769.jpg",
@@ -21,12 +15,17 @@ function App() {
     return (
         <div className="AGC">
             <HeaderComponent />
+            <StartSectionComponent />
+            <LearnITComponent />
+
             <div className="main w-full h-30 bg-gradient-to-r from-[#00052D] to-[#57033F]">
                 <div className="carousel-container mx-auto my-auto w-2/4 border-2 rounded-md ">
                     <Carousel slides={slides} />
                 </div>
             </div>
+            <ScopComponent/>
             <FooterComponent/>
+
         </div>
     );
 }
