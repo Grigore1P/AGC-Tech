8ebac2a1501be0e07ac56a209a45ec3d1778f513
aclin143 <caliniustin12@gmail.com>
2024-03-10 17:58:40 +0300

    header update

--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 
 const HeaderComponent = () => {
     return (
-        <div>
-            <div className="Header h-20 w-full flex items-center justify-between bg-gradient-to-r from-[#00052D] to-[#57033F] text-white text-xl">
 
-                <div className="Principale flex items-center space-x-4 ml-20">
-                    <div className="Header2 hover:underline">HOME</div>
-                    <div className="Header2 hover:underline">CONTACT</div>
-                    <div className="Header3 hover:underline">HELP</div>
-                </div>
-
-                <div className="logo hover:underline mr-6">LOGO</div>
+            <div className="Header h-20 w-full bg-gradient-to-r from-[#00052D] to-[#57003F] text-white text-2xl ">
+                <ul className="flex items-center justify-center space-x-20 relative">
+                    <li className="group Header1 border border-transparent p-2 ml-10 transition-all mt-5">
+                        <span className="hover:text-[#FC819E] border-[#891652] hover:border-4 hover:rounded-xl transition-all mt-2">HOME</span>
+                    </li>
+                    <li className="group Header2 border border-transparent p-2 ml-10 transition-all mt-5">
+                        <span className="hover:text-[#FC819E] border-[#891652] hover:border-4 hover:rounded-xl transition-all mt-2">COURSES</span>
+                    </li>
+                    <li className="group Header3 border border-transparent p-2 ml-10 transition-all mt-5">
+                        <span className="hover:text-[#FC819E] border-[#891652] hover:border-4 hover:rounded-xl transition-all mt-2">LOGO</span>
+                    </li>
+                    <li className="group Header4 border border-transparent p-2 ml-10 transition-all mt-5">
+                        <span className="hover:text-[#FC819E] border-[#891652] hover:border-4 hover:rounded-xl transition-all mt-2">ABOUT US</span>
+                    </li>
+                    <li className="group Header5 border border-transparent p-2 ml-10 transition-all mt-5">
+                        <span className="hover:text-[#FC819E] border-[#891652] hover:border-4 hover:rounded-xl transition-all mt-2">LOG IN</span>
+                    </li>
+                </ul>
             </div>
-        </div>
+
     );
 }
 
